d9c703a62640b64a7efdd0aa58007deeac3118ed
Rhesa Utomo <venderblitz@gmail.com>
2018-08-16 11:21:51 +0700

    add home, details layout + vue js components

---
+++ scripts/component/RecipeDetail.js
@@ -0,0 +1,29 @@
+Vue.component('recipe-detail', {
+  props: ['recipe'],
+  template: `
+  <div class="row">
+    <div class="col-sm-12">
+      <div class="card" style="margin-bottom: 1em">
+        <div class="card-body">
+          <h5 class="card-title">{{ recipe.title }}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Category: {{ recipe.category }}</h6>
+          <footer class="blockquote-footer mb-2">Created by: <i>{{ recipe.user }}</i></footer>
+        </div>
+        <div class="card-footer">
+          <a class="btn btn-primary" style="color: white">Edit</a>
+          <a class="btn btn-danger" style="color: white">Delete</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-sm-12">
+      <div class="card">
+        <div class="card-body">
+          <h6 class="card-title">How to make it</h6>
+          <div id="description" v-html="recipe.description"></div>
+        </div>
+        <img :src="recipe.imgUrl" alt="" class="card-img-bottom">
+      </div>
+    </div>
+  </div>
+  `
+})
